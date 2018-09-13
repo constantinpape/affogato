f6c099b2079098b6660c627bc0f80465123e74fd
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-09-13 10:34:54 +0200

    Implement affinity distance trafo

--- src/python/module/affogato/affinities/__init__.py
+++ src/python/module/affogato/affinities/__init__.py
@@ -1,1 +1,35 @@
+import numpy as np
 from ._affinities import *
+
+try:
+    from scipy import ndimage
+    WITH_SCIPY = True
+except ImportError:
+    WITH_SCIPY = False
+
+
+if WITH_SCIPY:
+
+    def affinity_distance_transform(affinities, clip_limit_short=100., clip_limit_long=30):
+        aff_distances = affinities.copy()
+
+        ndims = affinities.ndim - 1
+        nchannels = affinities.shape[0]
+
+        # invert the affinities
+        aff_distances = 1. - aff_distances
+
+        # invert the short range channels (why?)
+        aff_distances[0:ndims] = 1 - aff_distances[0:ndims]
+        # compute the distance transforms
+        for i in range(nchannels):
+            aff_distances[i] = ndimage.distance_transform_edt(aff_distances[i])
+
+        # TODO I don't understand what is going on here ...
+        aff_distances = -aff_distances
+        aff_distances[0:ndims] = 1 + np.clip(aff_distances[0:ndims], -clip_limit_short, None) / clip_limit_short
+        aff_distances[ndims:] = 1 + np.clip(aff_distances[ndims:], -clip_limit_long, None) / clip_limit_long
+
+        # I guess we need this only for mws ?!
+        aff_distances[ndims:] = 1 - aff_distances[ndims:]
+        return aff_distances
--- src/python/module/affogato/learning/__init__.py
+++ src/python/module/affogato/learning/__init__.py
@@ -3,7 +3,6 @@ from ._learning import *
 from ..segmentation import get_valid_edges
 
 
-# TODO expose pass parameter
 def mutex_malis(weights, gt_labels, offsets,
                 number_of_attractive_channels,
                 strides=None, randomize_strides=False,
@@ -21,6 +20,6 @@ def mutex_malis(weights, gt_labels, offsets,
     sorted_flat_indices = np.argsort(masked_weights, axis=None)[::-1]
 
     loss, grad, label_pos, labels_neg = mutex_malis_impl(weights.ravel(), sorted_flat_indices, valid_edges.ravel(),
-                                  gt_labels.copy().ravel(), offsets, number_of_attractive_channels,
-                                  image_shape, learn_in_ignore_label)
+                                                         gt_labels.copy().ravel(), offsets, number_of_attractive_channels,
+                                                         image_shape, learn_in_ignore_label)
     return loss, grad.reshape(weights.shape), label_pos.reshape(gt_labels.shape), labels_neg.reshape(gt_labels.shape)
