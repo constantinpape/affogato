9c7de47f0d784cfd76b24b64d3ee4f8c57751796
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-01-17 10:50:16 +0100

    Export affinity functions for int64 as well

--- src/python/lib/affinities/affinities.cxx
+++ src/python/lib/affinities/affinities.cxx
@@ -10,6 +10,75 @@
 
 namespace py = pybind11;
 
+namespace affogato {
+
+    template<typename T>
+    void export_affinities_T(py::module & m) {
+        m.def("compute_affinities", [](const xt::pyarray<T> & labels,
+                                       const std::vector<std::vector<int>> & offsets,
+                                       const bool have_ignore_label,
+                                       const T ignore_label) {
+                // compute the out shape
+                typedef typename xt::pyarray<float>::shape_type ShapeType;
+                const auto & shape = labels.shape();
+                const unsigned ndim = labels.dimension();
+                ShapeType out_shape(ndim + 1);
+                out_shape[0] = offsets.size();
+                for(unsigned d = 0; d < ndim; ++d) {
+                    out_shape[d + 1] = shape[d];
+                }
+
+                // allocate the output
+                xt::pyarray<float> affs = xt::zeros<float>(out_shape);
+                xt::pyarray<uint8_t> mask = xt::zeros<uint8_t>(out_shape);
+                {
+                    py::gil_scoped_release allowThreads;
+                    affinities::compute_affinities(labels, offsets,
+                                                   affs, mask,
+                                                   have_ignore_label, ignore_label);
+                }
+                return std::make_pair(affs, mask);
+            }, py::arg("labels").noconvert(),
+               py::arg("offset"),
+               py::arg("have_ignore_label")=false,
+               py::arg("ignore_label")=0);
+    }
+
+    //
+    template<typename T>
+    void export_multiscale_affinities_T(py::module & m) {
+        m.def("compute_multiscale_affinities", [](const xt::pyarray<T> & labels,
+                                                  const std::vector<int> & block_shape,
+                                                  const bool have_ignore_label,
+                                                  const T ignore_label) {
+                // compute the out shape
+                typedef typename xt::pyarray<float>::shape_type ShapeType;
+                const auto & shape = labels.shape();
+                const unsigned ndim = shape.size();
+                ShapeType out_shape(ndim + 1);
+                out_shape[0] = ndim;
+                for(unsigned d = 0; d < ndim; ++d) {
+                    // integer division should do the right thing in all cases
+                    out_shape[d + 1] = (shape[d] % block_shape[d]) ? shape[d] / block_shape[d] + 1 : shape[d] / block_shape[d];
+                }
+
+                // allocate the output
+                xt::pyarray<float> affs = xt::zeros<float>(out_shape);
+                xt::pyarray<uint8_t> mask = xt::zeros<float>(out_shape);
+                {
+                    py::gil_scoped_release allowThreads;
+                    affinities::compute_multiscale_affinities(labels, block_shape,
+                                                              affs, mask,
+                                                              have_ignore_label, ignore_label);
+                }
+                return std::make_pair(affs, mask);
+            }, py::arg("labels"),
+               py::arg("block_shape"),
+               py::arg("have_ignore_label")=false,
+               py::arg("ignore_label")=0);
+    }
+}
+
 PYBIND11_MODULE(_affinities, m)
 {
     xt::import_numpy();
@@ -17,63 +86,9 @@ PYBIND11_MODULE(_affinities, m)
 
     using namespace affogato;
 
-    m.def("compute_multiscale_affinities", [](const xt::pyarray<uint64_t> & labels,
-                                              const std::vector<int> & block_shape,
-                                              const bool have_ignore_label,
-                                              const uint64_t ignore_label) {
-            // compute the out shape
-            typedef typename xt::pyarray<float>::shape_type ShapeType;
-            const auto & shape = labels.shape();
-            const unsigned ndim = shape.size();
-            ShapeType out_shape(ndim + 1);
-            out_shape[0] = ndim;
-            for(unsigned d = 0; d < ndim; ++d) {
-                // integer division should do the right thing in all cases
-                out_shape[d + 1] = (shape[d] % block_shape[d]) ? shape[d] / block_shape[d] + 1 : shape[d] / block_shape[d];
-            }
-
-            // allocate the output
-            xt::pyarray<float> affs = xt::zeros<float>(out_shape);
-            xt::pyarray<uint8_t> mask = xt::zeros<float>(out_shape);
-            {
-                py::gil_scoped_release allowThreads;
-                affinities::compute_multiscale_affinities(labels, block_shape,
-                                                          affs, mask,
-                                                          have_ignore_label, ignore_label);
-            }
-            return std::make_pair(affs, mask);
-        }, py::arg("labels"),
-           py::arg("block_shape"),
-           py::arg("have_ignore_label")=false,
-           py::arg("ignore_label")=0);
-
-
-    m.def("compute_affinities", [](const xt::pyarray<uint64_t> & labels,
-                                   const std::vector<std::vector<int>> & offsets,
-                                   const bool have_ignore_label,
-                                   const uint64_t ignore_label) {
-            // compute the out shape
-            typedef typename xt::pyarray<float>::shape_type ShapeType;
-            const auto & shape = labels.shape();
-            const unsigned ndim = labels.dimension();
-            ShapeType out_shape(ndim + 1);
-            out_shape[0] = offsets.size();
-            for(unsigned d = 0; d < ndim; ++d) {
-                out_shape[d + 1] = shape[d];
-            }
-
-            // allocate the output
-            xt::pyarray<float> affs = xt::zeros<float>(out_shape);
-            xt::pyarray<uint8_t> mask = xt::zeros<uint8_t>(out_shape);
-            {
-                py::gil_scoped_release allowThreads;
-                affinities::compute_affinities(labels, offsets,
-                                               affs, mask,
-                                               have_ignore_label, ignore_label);
-            }
-            return std::make_pair(affs, mask);
-        }, py::arg("labels"),
-           py::arg("offset"),
-           py::arg("have_ignore_label")=false,
-           py::arg("ignore_label")=0);
+    export_affinities_T<uint64_t>(m);
+    export_affinities_T<int64_t>(m);
+    
+    export_multiscale_affinities_T<uint64_t>(m);
+    export_multiscale_affinities_T<int64_t>(m);
 }
