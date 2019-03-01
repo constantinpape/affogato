1bd936d90ad1233140ca725a9e09091e5d0261ad
Steffen Wolf <steffen.wolf@iwr.uni-heidelberg.de>
2019-03-01 13:32:09 +0100

    fix init file

--- src/python/module/affogato/segmentation/__init__.py
+++ src/python/module/affogato/segmentation/__init__.py
@@ -1,4 +1,4 @@
 from ._segmentation import connected_components, compute_zws_segmentation, compute_mws_clustering, MWSGridGraph
 from .mws import compute_mws_segmentation
 from .causal_mws import compute_causal_mws
-from .semantic_mws import compute_semantic_mws
+from .semantic_mws import compute_semantic_mws_segmentation, compute_semantic_mws_clustering
