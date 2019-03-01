2f48e2f50d8456e6288778519868b29bd5a2feb3
Steffen Wolf <steffen.wolf@iwr.uni-heidelberg.de>
2019-03-01 13:22:59 +0100

    clean up semantic mws bindings

--- src/python/module/affogato/segmentation/__init__.py
+++ src/python/module/affogato/segmentation/__init__.py
@@ -1,3 +1,4 @@
-from ._segmentation import connected_components, compute_zws_segmentation, compute_mws_clustering
+from ._segmentation import connected_components, compute_zws_segmentation, compute_mws_clustering, MWSGridGraph
 from .mws import compute_mws_segmentation
 from .causal_mws import compute_causal_mws
+from .semantic_mws import compute_semantic_mws
