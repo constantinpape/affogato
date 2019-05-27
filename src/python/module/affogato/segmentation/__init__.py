be743ef2956e38341bffb2555bc8ed8132a7a645
Constantin Pape <c.pape@gmx.net>
2019-05-27 16:39:00 +0200

    Soften vigra dependency

--- src/python/module/affogato/segmentation/__init__.py
+++ src/python/module/affogato/segmentation/__init__.py
@@ -1,5 +1,6 @@
 from ._segmentation import connected_components, compute_zws_segmentation, compute_mws_clustering, MWSGridGraph
 from .mws import compute_mws_segmentation
-from .causal_mws import compute_causal_mws
 from .semantic_mws import compute_semantic_mws_segmentation, compute_semantic_mws_clustering
 from .interactive_mws import InteractiveMWS
+# NOTE we don't include the causal mws here, because it still relies on vigra
+# from .causal_mws import compute_causal_mws
