20a57c9db31b26b7877a6c2c8faa3522e883a36a
Alberto Bailoni <bailoni.alberto@gmail.com>
2021-01-04 17:41:31 +0100

    Add single linkage clustering

--- src/python/module/affogato/segmentation/__init__.py
+++ src/python/module/affogato/segmentation/__init__.py
@@ -1,5 +1,5 @@
 from ._segmentation import connected_components, compute_zws_segmentation
-from ._segmentation import compute_mws_clustering, MWSGridGraph
+from ._segmentation import compute_mws_clustering, MWSGridGraph, compute_single_linkage_clustering
 from .mws import compute_mws_segmentation
 from .semantic_mws import compute_semantic_mws_segmentation, compute_semantic_mws_clustering
 from .interactive_mws import InteractiveMWS
