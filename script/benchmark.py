589ac18a5891e54ff26ac9fc92cb0e3fb1cab1cd
Constantin Pape <c.pape@gmx.net>
2021-11-19 18:46:41 +0100

    Add benchmark script WIP

---
+++ script/benchmark.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def benchmark(path):
+    pass
+
+
+if __name__ == "__main__":
+    # The example data from https://oc.embl.de/index.php/s/sXJzYVK0xEgowOz
+    path = "/home/pape/Work/data/isbi/isbi_test_volume.h5"
+    benchmark(path)
