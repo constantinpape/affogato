827c1e6a8c522b5f6e8aba55aa5469173477c442
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-05-06 19:27:41 +0200

    Update build script

--- .github/workflows/build_unix.sh
+++ .github/workflows/build_unix.sh
@@ -4,7 +4,7 @@ export PY_BIN="$CONDA_PREFIX/bin/python"
 cmake . \
     -DBUILD_PYTHON=ON \
     -DCMAKE_PREFIX_PATH="$CONDA_PREFIX" \
-    -DPYTHON_EXECUTABLE="$PY_BIN" \
+    -DPython_EXECUTABLE="${CONDA_PREFIX}/bin/python" \
     -DCMAKE_CXX_FLAGS="-std=c++17" \
     -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX"
 make -j 4
