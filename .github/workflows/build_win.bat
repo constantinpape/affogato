2ff357f9ca8959b8738cd289d46427fed4265dbc
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-05-12 23:39:42 +0200

    Try to fix win build

--- .github/workflows/build_win.bat
+++ .github/workflows/build_win.bat
@@ -1,12 +1,6 @@
-for /f "usebackq delims=" %%I in (`python -c "import numpy; print(numpy.get_include())"`) do (
-    set "NUMPY_INCLUDE_DIR=%%I"
-)
-
 cmake . -G "NMake Makefiles" ^
     -DBUILD_PYTHON=ON ^
-    -DCMAKE_PREFIX_PATH="%CONDA_PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX="%CONDA_PREFIX%" ^
-    -DPYTHON_EXECUTABLE="%PYTHON%" ^
-    -DPython3_NumPy_INCLUDE_DIR=%NUMPY_INCLUDE_DIR% ^
-    -DPython_NumPy_INCLUDE_DIR=%NUMPY_INCLUDE_DIR%
+    -DCMAKE_PREFIX_PATH:PATH="%CONDA_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%" ^
+    -DPython_EXECUTABLE:PATH="%CONDA_PREFIX%\python.exe" ^
 cmake --build . --target INSTALL --config Release -j 4
