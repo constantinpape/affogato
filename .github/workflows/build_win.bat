728649a95ab813b256232aed876a9618d26cabab
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-05-12 23:46:51 +0200

    Try to fix windows build

--- .github/workflows/build_win.bat
+++ .github/workflows/build_win.bat
@@ -3,4 +3,4 @@ cmake . -G "NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH:PATH="%CONDA_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%" ^
     -DPython_EXECUTABLE:PATH="%CONDA_PREFIX%\python.exe" ^
-cmake --build . --target INSTALL --config Release -j 4
+cmake --build . --target INSTALL --config Release
