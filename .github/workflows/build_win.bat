3141c634eb735516981d7ab19113486a6940dbb6
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-05-12 23:30:04 +0200

    Update boost dependency and try to fix windows build

--- .github/workflows/build_win.bat
+++ .github/workflows/build_win.bat
@@ -1,6 +1,12 @@
+for /f "usebackq delims=" %%I in (`python -c "import numpy; print(numpy.get_include())"`) do (
+    set "NUMPY_INCLUDE_DIR=%%I"
+)
+
 cmake . -G "NMake Makefiles" ^
     -DBUILD_PYTHON=ON ^
     -DCMAKE_PREFIX_PATH="%CONDA_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%CONDA_PREFIX%" ^
-    -DPYTHON_EXECUTABLE="%PYTHON%"
+    -DPYTHON_EXECUTABLE="%PYTHON%" ^
+    -DPython3_NumPy_INCLUDE_DIR=%NUMPY_INCLUDE_DIR% ^
+    -DPython_NumPy_INCLUDE_DIR=%NUMPY_INCLUDE_DIR%
 cmake --build . --target INSTALL --config Release -j 4
